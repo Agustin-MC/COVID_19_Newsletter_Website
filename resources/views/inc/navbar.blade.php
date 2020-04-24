810e56d370c8c6c1c3755835d3db1daa7a82ce45
Agustin <agustinmanriquezcruz@outlook.com>
2020-04-24 00:02:06 -0500

    Designing Home Page

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -4,12 +4,12 @@
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="/">Home <span class="sr-only"></span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item active">
                 <a class="nav-link" href="/about">About</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item active">
                 <a class="nav-link" href="/contact">Contact</a>
             </li>
         </ul>
---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron text-center">
+    <div class="container">
+        <h1>Welcome to the COVID 19 Newsletter Reporter</h1>
+        <p class="lead"> In times like this, we are here to help you stay informed on the latest developments of COVID 19.</p>
+    </div>
+</div>
