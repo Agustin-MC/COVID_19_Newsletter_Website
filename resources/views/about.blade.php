3e9b26ddd1e006d82c2caf9e646a75c2c3ae01bc
Agustin <agustinmanriquezcruz@outlook.com>
2020-04-23 23:38:45 -0500

    Testing How it's going to look with content

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 @section('content')
     <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda ipsa, iste minus nihil nulla quidem vel velit voluptates! Animi asperiores deleniti inventore magnam nemo provident quod veritatis. Deleniti, id, nemo.</p>
+
 @endsection
 
-
+@section('sidebar')
+    @parent
+    <p>This is appended to the sidebar</p>
+@endsection
