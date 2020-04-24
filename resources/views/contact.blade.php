6846381cbd714ef212dce2590542b7ac0257483d
Agustin <agustinmanriquezcruz@outlook.com>
2020-04-24 00:37:38 -0500

    Designing Contact Front End

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 @section('content')
     <h1>Contact</h1>
+    {!! Form::open(['url' => 'contact/submit']) !!}
+        <div clas="form-group">
+            {{Form::label('name', 'Name')}}
+            {{Form::text('name','',['class' => 'form-control', 'placeholder' => 'Enter name'])}}
+        </div>
+        <div clas="form-group">
+            {{Form::label('email', 'E-Mail Address')}}
+            {{Form::text('email', '',['class' => 'form-control', 'placeholder' => 'johnsmith@example.com'])}}
+        </div>
+        <div clas="form-group">
+            {{Form::label('message', 'Share Your Story')}}
+            {{Form::textarea('message', '',['class' => 'form-control', 'placeholder' => 'Your Message'])}}
+        </div>
+        <div>
+            {{Form::submit('Submit',['class'=> 'btn btn-primary'])}}
+        </div>
+    {!! Form::close() !!}
 @endsection
 
 
