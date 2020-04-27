a760bb1355f1b3dbb8058d1339d66b13b4750cd2
Agustin <agustinmanriquezcruz@outlook.com>
2020-04-26 23:05:53 -0500

    Embedded twitter to my home page

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -1,6 +1,8 @@
 @section('sidebar')
     <div class="card card-body bg-light">
-        <h3>Sidebar</h3>
-        This is the sidebar
+        <h3>CDC'S Latest Stories</h3>
+        <a class="twitter-timeline" data-height="500" href="https://twitter.com/CDCgov?ref_src=twsrc%5Etfw">
+            Tweets by CDCgov
+        </a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
     @show
